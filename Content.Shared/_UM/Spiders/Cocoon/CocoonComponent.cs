086af31f90766996b33e75debc237d8d8773bbca
jessicamaybe <jessica@jessicamaybe.com>
2026-02-02 14:44:52 -0800

    destroyed cocoon drop contents, reduce doafter time

--- Content.Shared/_UM/Spiders/Cocoon/CocoonComponent.cs
+++ Content.Shared/_UM/Spiders/Cocoon/CocoonComponent.cs
@@ -56,7 +56,7 @@ public sealed partial class CocoonComponent : Component
     /// How long should it take to absorb?
     /// </summary>
     [DataField]
-    public TimeSpan Duration = TimeSpan.FromSeconds(30);
+    public TimeSpan Duration = TimeSpan.FromSeconds(15);
 
     /// <summary>
     /// Maximum energy this cocoon can provide
