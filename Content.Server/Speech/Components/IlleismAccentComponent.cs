9a1487aec8560816298da4156b29f2eb23c7e0b9
TriviaSolari <154280615+TriviaSolari@users.noreply.github.com>
2026-02-09 19:47:29 -0500

    Add comment to datafield

--- Content.Server/Speech/Components/IlleismAccentComponent.cs
+++ Content.Server/Speech/Components/IlleismAccentComponent.cs
@@ -6,6 +6,10 @@ namespace Content.Server.Speech.Components;
 [Access(typeof(IlleismAccentSystem))]
 public sealed partial class IlleismAccentComponent : Component
 {
+    /// <summary>
+    /// The strings that should be used to split the character's name.
+    /// Only the part up to the first occurrence is kept.
+    /// </summary>
     [DataField]
     public List<string> SplitOnStrings = new();
 }
