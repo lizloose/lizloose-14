d5c6b2ec283cec209cbf2ea14677dffa62c6a6ab
jessicamaybe <jessica@jessicamaybe.com>
2026-02-25 18:57:26 -0800

    comment

--- Content.Shared/_UM/Cargo/Components/UMCargoShuttleComponent.cs
+++ Content.Shared/_UM/Cargo/Components/UMCargoShuttleComponent.cs
@@ -14,6 +14,10 @@ public sealed partial class UMCargoShuttleComponent : Component
     [DataField]
     public List<CargoOrderData> CurrentOrders = new();
 
+    /// <summary>
+    /// if we still need to do the initial warp
+    /// (warp to station while in station space)
+    /// </summary>
     [DataField]
     public bool FirstWarp = true;
 
